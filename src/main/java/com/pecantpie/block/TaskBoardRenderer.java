1f6f3f1781bb5d16777e285fd9b4e14537316df2
Pecant-Pie <47540913+Pecant-Pie@users.noreply.github.com>
2025-10-07 23:22:53 -0400

    Make text wrap on task boards

--- src/main/java/com/pecantpie/block/TaskBoardRenderer.java
+++ src/main/java/com/pecantpie/block/TaskBoardRenderer.java
@@ -11,15 +11,18 @@ import net.minecraft.client.renderer.entity.DisplayRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
 import net.minecraft.util.ColorRGBA;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.StandingSignBlock;
 import net.minecraft.world.level.block.entity.SignText;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3f;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 import static com.pecantpie.block.TaskBoardBlock.FACING;
@@ -27,7 +30,7 @@ import static com.pecantpie.block.TaskBoardBlock.FACING;
 public class TaskBoardRenderer implements BlockEntityRenderer<TaskBoardBlockEntity> {
     private static final int TEXT_COLOR = DyeColor.BLACK.getTextColor();
     private static final int LINE_HEIGHT = 10;
-    private static final int MAX_WIDTH = 90;
+    private static final int MAX_WIDTH = 83;
     private static final int MAX_LINES = 4;
     private static final float TEXT_SCALE = 0.012f;
 
@@ -41,30 +44,38 @@ public class TaskBoardRenderer implements BlockEntityRenderer<TaskBoardBlockEnti
     public void render(TaskBoardBlockEntity taskBoardBlockEntity, float pPartialTick, PoseStack poseStack, MultiBufferSource multiBufferSource, int pPackedLight, int pPackedOverlay) {
         poseStack.pushPose();
         centerText(poseStack, taskBoardBlockEntity.getBlockState());
+
+        // render Task text. This should fit nicely as long as the task's name is 52 or less characters.
+        // The default item name limit in the anvil is 50, so we should stick to that.
         poseStack.scale(TEXT_SCALE, -TEXT_SCALE, TEXT_SCALE);
-        renderText(taskBoardBlockEntity.getBlockPos(), taskBoardBlockEntity.getTaskName(), poseStack, multiBufferSource, pPackedLight);
+        renderText(taskBoardBlockEntity.getTaskName(), poseStack, multiBufferSource, pPackedLight, null, MAX_WIDTH, MAX_LINES);
+
         poseStack.popPose();
     }
-    
+
     void centerText(PoseStack poseStack, BlockState state) {
         Vector3f offset = state.getValue(FACING).step().mul(0.5125f); // just over 1 total so that it barely sticks out
         poseStack.translate(offset.x + 0.5f, 0.5f, offset.z + 0.5f);
         poseStack.mulPose(Axis.YN.rotationDegrees(state.getValue(FACING).toYRot()));
     }
 
-    void renderText(BlockPos pos, Component text, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+    void renderText(Component text, PoseStack poseStack, MultiBufferSource buffer, int packedLight, @Nullable Vec2 offset, int maxWidth, int maxLines) {
         poseStack.pushPose();
 
-        int j = MAX_LINES * LINE_HEIGHT / 2;
-        FormattedCharSequence aformattedcharsequence = text.getVisualOrderText();
+        offset = offset == null ? Vec2.ZERO : offset;
 
+        int midline = maxLines * LINE_HEIGHT / 2;
 
 //        for(int i1 = 0; i1 < 4; ++i1) {
-//            FormattedCharSequence formattedcharsequence = aformattedcharsequence;
-            float f = (float)(-this.font.width(aformattedcharsequence) / 2);
+        int line = 0;
+        for(FormattedCharSequence formattedcharsequence : font.split(FormattedText.of(text.getString()), maxWidth)) {
+            float f = (float)(-this.font.width(formattedcharsequence) / 2);
 
-            this.font.drawInBatch(aformattedcharsequence, f, (float)(0 * LINE_HEIGHT - j), TEXT_COLOR, false, poseStack.last().pose(), buffer, Font.DisplayMode.POLYGON_OFFSET, 0, packedLight);
-//        }
+            this.font.drawInBatch(formattedcharsequence, f + offset.x, (float)(line * LINE_HEIGHT - midline) + offset.y, TEXT_COLOR, false, poseStack.last().pose(), buffer, Font.DisplayMode.POLYGON_OFFSET, 0, packedLight);
+            line++;
+            if (line >= maxLines)
+                break;
+        }
 
         poseStack.popPose();
     }
